f9c204f08a222109ee8584fd29751ebfea824be7
Musaib Altaf <musaibaltafurahman@gmail.com>
2021-10-29 22:52:02 +0530

    hosts commit

--- ITScorer/settings.py
+++ ITScorer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9(z=476wa5_v6g#5=vb%cel=xhr8en1s)-q00d5yh&t+4o@v+q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://musaib-social.herokuapp.com/', 'musaib-social.herokuapp.com/']
 
 
 # Application definition
